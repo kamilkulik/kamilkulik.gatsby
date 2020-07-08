08a8cb427ac5d373b32d27c5741116c06c994ab9
Kamil <37276273+kamilkulik@users.noreply.github.com>
2020-07-08 17:46:46 +0200

    yet another inefficient carousel solution

--- src/components/ProjectManagement/Carousel.js
+++ src/components/ProjectManagement/Carousel.js
@@ -10,12 +10,52 @@ const Carousel = () => {
   const pageCount = Stories.length - 1
 
   const changePage = direction => () => {
+    console.log(direction)
     setDirection(direction)
     if (direction === "next" && page < pageCount) setPage(page + 1)
     else if (direction === "previous" && page > 0) setPage(page - 1)
   }
 
   const currentPage = [Stories[page]]
+
+  const getNodesArray = () => {
+    const target = document.getElementsByClassName("carouselPage")
+    const nodesArray = Array.from(target)
+    return nodesArray
+  }
+
+  const removeDone = () => {
+    const nodes = getNodesArray()
+    const previousClass = "previous-enter-done"
+    const nextClass = "next-enter-done"
+    nodes.forEach(node => {
+      if (node.classList.contains(previousClass))
+        node.classList.remove(previousClass)
+      else if (node.classList.contains(nextClass))
+        node.classList.remove(nextClass)
+    })
+  }
+
+  const adjustDirection = () => {
+    const nodes = getNodesArray()
+    const previousClass = "previous-exit"
+    const nextClass = "next-exit"
+    const previousClassActive = "previous-exit-active"
+    const nextClassActive = "next-exit-active"
+    console.log("adjust")
+    nodes.forEach(node => {
+      if (node.classList.contains(previousClass) && direction === "next") {
+        console.log("replacing previous")
+        node.classList.replace(previousClass, nextClass)
+        node.classList.replace(previousClassActive, nextClassActive)
+      }
+      if (node.classList.contains(nextClass) && direction === "previous") {
+        console.log("replacing next")
+        node.classList.replace(nextClass, previousClass)
+        node.classList.replace(nextClassActive, previousClassActive)
+      }
+    })
+  }
 
   return (
     <div className="carousel">
@@ -25,8 +65,10 @@ const Carousel = () => {
           {currentPage.map(story => (
             <CSSTransition
               key={story.flag}
-              timeout={1000}
-              classNames={`carouselFly`}
+              timeout={10000}
+              onEntered={removeDone}
+              onExiting={adjustDirection}
+              classNames={`${direction}`}
             >
               {<CarouselPage story={story} key={story.flag} />}
             </CSSTransition>
