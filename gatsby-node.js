352d160f9b2ebe42aac33c513e35600cf71833c1
Kamil Kulik <kamil.kulik@heineken.com>
2026-07-04 07:23:27 +0200

    resolve build warnings

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,18 +2,23 @@ const path = require("path")
 
 const ICONS_PATH = path.resolve(__dirname, "src/images/icons")
 
+const excludeIconsPath = rule => {
+  if (rule.test && rule.test.toString().includes("svg")) {
+    rule.exclude = rule.exclude
+      ? [].concat(rule.exclude, ICONS_PATH)
+      : ICONS_PATH
+  }
+}
+
 exports.onCreateWebpackConfig = ({ actions, getConfig }) => {
   const config = getConfig()
 
   config.module.rules.forEach(rule => {
-    if (!rule.oneOf) return
-    rule.oneOf.forEach(one => {
-      if (one.test && one.test.toString().includes("svg")) {
-        one.exclude = one.exclude
-          ? [].concat(one.exclude, ICONS_PATH)
-          : ICONS_PATH
-      }
-    })
+    if (rule.oneOf) {
+      rule.oneOf.forEach(excludeIconsPath)
+    } else {
+      excludeIconsPath(rule)
+    }
   })
 
   actions.replaceWebpackConfig(config)
