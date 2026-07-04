58a5b8a91ffa3ce7732186db1d47828fe13d4e72
Kamil Kulik <kamil.kulik@heineken.com>
2026-07-04 07:35:12 +0200

    clickable

--- src/components/Buttons/Button.js
+++ src/components/Buttons/Button.js
@@ -2,16 +2,14 @@ import React from "react"
 
 const Button = ({ url, title, propStyle }) => {
   return (
-    <button className={`button ${propStyle}`}>
-      <a
-        href={url}
-        target="_blank"
-        rel="noopener noreferrer"
-        className="button__text"
-      >
-        {title}
-      </a>
-    </button>
+    <a
+      href={url}
+      target="_blank"
+      rel="noopener noreferrer"
+      className={`button button__text ${propStyle || ""}`}
+    >
+      {title}
+    </a>
   )
 }
 
